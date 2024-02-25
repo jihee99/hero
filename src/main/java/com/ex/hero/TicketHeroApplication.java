d68e804fd1c18ead98f444d14bcd127f1c1f3ea6
jihee99 <oosoojh31@gmail.com>
2024-02-25 15:07:33 +0900

    Initial commit

---
+++ src/main/java/com/ex/hero/TicketHeroApplication.java
@@ -0,0 +1,13 @@
+package com.ex.hero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ex/hero/TicketHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.hero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
