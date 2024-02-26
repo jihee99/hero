3c6c6d5f23f81b51266fe0c33c3d71d336f232a3
jihee99 <oosoojh31@gmail.com>
2024-02-26 17:48:43 +0900

    feat: 프로퍼티 세팅 변경

--- src/main/java/com/ex/hero/AdminInitializer.java
+++ src/main/java/com/ex/hero/AdminInitializer.java
@@ -1,33 +1,33 @@
-package com.ex.hero;
-
-import java.time.LocalDateTime;
-
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import com.ex.hero.member.model.Member;
-import com.ex.hero.member.model.MemberType;
-import com.ex.hero.member.repository.MemberRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Component
-public class AdminInitializer implements ApplicationRunner {
-    private final MemberRepository memberRepository;
-
-	private final PasswordEncoder passwordEncoder;
-
-    @Override
-    public void run(ApplicationArguments args) {
-        memberRepository.save(Member.builder()
-                .account("admin")
-                .password(passwordEncoder.encode("admin"))
-                .name("관리자")
-                .role(MemberType.ADMIN)
-                .createdAt(LocalDateTime.now())
-                .build());
-    }
-}
+// package com.ex.hero;
+//
+// import java.time.LocalDateTime;
+//
+// import org.springframework.boot.ApplicationArguments;
+// import org.springframework.boot.ApplicationRunner;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.stereotype.Component;
+//
+// import com.ex.hero.member.model.Member;
+// import com.ex.hero.member.model.MemberType;
+// import com.ex.hero.member.repository.MemberRepository;
+//
+// import lombok.RequiredArgsConstructor;
+//
+// @RequiredArgsConstructor
+// @Component
+// public class AdminInitializer implements ApplicationRunner {
+//     private final MemberRepository memberRepository;
+//
+// 	private final PasswordEncoder passwordEncoder;
+//
+//     @Override
+//     public void run(ApplicationArguments args) {
+//         memberRepository.save(Member.builder()
+//                 .account("admin")
+//                 .password(passwordEncoder.encode("admin"))
+//                 .name("관리자")
+//                 .role(MemberType.ADMIN)
+//                 .createdAt(LocalDateTime.now())
+//                 .build());
+//     }
+// }
