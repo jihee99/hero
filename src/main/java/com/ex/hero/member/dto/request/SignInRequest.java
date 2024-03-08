9cf7d67292076456150e552bd2b681d406efd09f
손지희 <wlhee@ejbt.co.kr>
2024-03-08 10:22:16 +0900

    feat : 스키마 수정

--- src/main/java/com/ex/hero/member/dto/request/SignInRequest.java
+++ src/main/java/com/ex/hero/member/dto/request/SignInRequest.java
@@ -3,7 +3,7 @@ package com.ex.hero.member.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record SignInRequest(
-        @Schema(description = "회원 이메일", example = "user@user.com")
+        @Schema(description = "회원 이메일", example = "user@test.com")
         String email,
         @Schema(description = "회원 비밀번호", example = "1234")
         String password
