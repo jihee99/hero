29a22fa3e2f4cac10817733f0811abad9b4b2946
손지희 <wlhee@ejbt.co.kr>
2024-03-08 14:37:37 +0900

    feat : 그룹 발행 이벤트 조회 서비스 추가

---
+++ src/main/java/com/ex/hero/domains/common/vo/EventBasicVo.java
@@ -0,0 +1,30 @@
+package com.ex.hero.domains.common.vo;
+
+import com.ex.hero.events.model.Event;
+import com.ex.hero.events.model.EventBasic;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class EventBasicVo {
+    private String name;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private Long runTime;
+
+    public static EventBasicVo from(Event event) {
+        EventBasic eventBasic = event.getEventBasic();
+        if (eventBasic == null) {
+            return EventBasicVo.builder().build();
+        }
+        return EventBasicVo.builder()
+                .name(eventBasic.getName())
+                .startAt(eventBasic.getStartAt())
+                .endAt(event.getEndAt())
+                .runTime(eventBasic.getRunTime())
+                .build();
+    }
+}
