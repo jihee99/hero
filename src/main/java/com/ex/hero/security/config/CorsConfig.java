7ddf82f2a46d92ec81677b3e7098ae3724af496f
손지희 <wlhee@ejbt.co.kr>
2024-03-21 11:32:17 +0900

    feat: cors configuration 추가

---
+++ src/main/java/com/ex/hero/security/config/CorsConfig.java
@@ -0,0 +1,38 @@
+package com.ex.hero.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = getCorsConfiguration();
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }
+
+    private CorsConfiguration getCorsConfiguration() {
+        CorsConfiguration configuration =  new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowedMethods(getHttpMethods());
+        return configuration;
+    }
+
+    private List<String> getHttpMethods() {
+        return List.of(
+            HttpMethod.GET.name(), HttpMethod.POST.name(),
+            HttpMethod.PUT.name(), HttpMethod.OPTIONS.name()
+        );
+    }
+}
